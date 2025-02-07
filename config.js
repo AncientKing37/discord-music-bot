63eb787a1db5087b0c9afb717b93a28fda475cc4
AncientKing37 <76946951+AncientKing37@users.noreply.github.com>
2025-02-07 20:20:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["1093490767311405087", ""], 
+  mongodbUri : "mongodb+srv://kurinoxd:<db_password>@cluster0.31l21.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/yfCkJqmUwc",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
